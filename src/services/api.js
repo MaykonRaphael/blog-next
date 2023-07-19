288c10c5e4437e7877766b7c90c017a5592877c4
Maykon Raphael <mraphaeld.batista@gmail.com>
2023-07-19 17:55:23 -0300

    feat: update api key to .env

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const api = axios.create({
     baseURL: 'https://kgwqjmmvgqgdkortppow.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtnd3FqbW12Z3FnZGtvcnRwcG93Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4OTEwNzI2NywiZXhwIjoyMDA0NjgzMjY3fQ.mYpH2cDcItJM3QcMA3yNsl5uYYcmE_sKG4NoOPpX-BM",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtnd3FqbW12Z3FnZGtvcnRwcG93Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4OTEwNzI2NywiZXhwIjoyMDA0NjgzMjY3fQ.mYpH2cDcItJM3QcMA3yNsl5uYYcmE_sKG4NoOPpX-BM"
+        apikey: process.env.API_KEY,
+        authorization: process.env.AUTHORIZATION
     }
 })
